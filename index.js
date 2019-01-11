957b81f1b5cf31124f67beee3ee38d6918ccceec
patbpm <patbpm1@gmail.com>
2019-01-11 14:58:29 +0200

    Create a an interactive using Yarks

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
 
-let request = require('request');
+const request = require('request');
+const argv = require('yargs').argv;
 
 let apiKey = 'e6555ca496e59cd15af0a34b0edab960';
-let city = 'portland';
-let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`
-
+let city = argv.c || 'portland';
+let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}`
 
 request(url, function (err, response, body) {
   if(err){
     console.log('error:', error);
   } else {
-    console.log('body:', body);
+    let weather = JSON.parse(body)
+    let message = `It's ${weather.main.temp} degrees in ${weather.name}!`;
+    console.log(message);
   }
 });
